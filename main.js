59f4b4c2b7bd8150d92e8cd8cdd6df1bdc3ef28e
Dan King <king.daniel.thomas@gmail.com>
2023-06-03 15:25:55 +0700

    broaden import/export chains

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /*
- * ATTENTION: An "eval-source-map" devtool has been used.
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
  * This devtool is neither made for production nor for readable output files.
- * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
  * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
  * or disable the default devtool with "devtool: false".
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
@@ -16,7 +16,7 @@
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _load_tab_one__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./load-tab-one */ \"./src/load-tab-one.js\");\n\n\nconst body = document.getElementsByTagName('body');\nconst header = document.getElementsByTagName('header');\nconst main = document.getElementsByTagName('main');\nconst footer = document.getElementsByTagName('footer');\n\nconst content = document.getElementById('content');\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvaW5kZXguanMuanMiLCJtYXBwaW5ncyI6Ijs7QUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vZ2VuZXJhdGVkLXNpdGUtdG9wLy4vc3JjL2luZGV4LmpzP2I2MzUiXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHRhYk9uZSBmcm9tICcuL2xvYWQtdGFiLW9uZSc7XG5cbmNvbnN0IGJvZHkgPSBkb2N1bWVudC5nZXRFbGVtZW50c0J5VGFnTmFtZSgnYm9keScpO1xuY29uc3QgaGVhZGVyID0gZG9jdW1lbnQuZ2V0RWxlbWVudHNCeVRhZ05hbWUoJ2hlYWRlcicpO1xuY29uc3QgbWFpbiA9IGRvY3VtZW50LmdldEVsZW1lbnRzQnlUYWdOYW1lKCdtYWluJyk7XG5jb25zdCBmb290ZXIgPSBkb2N1bWVudC5nZXRFbGVtZW50c0J5VGFnTmFtZSgnZm9vdGVyJyk7XG5cbmNvbnN0IGNvbnRlbnQgPSBkb2N1bWVudC5nZXRFbGVtZW50QnlJZCgnY29udGVudCcpO1xuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./src/index.js\n");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   body: () => (/* binding */ body),\n/* harmony export */   content: () => (/* binding */ content),\n/* harmony export */   footer: () => (/* binding */ footer),\n/* harmony export */   header: () => (/* binding */ header),\n/* harmony export */   main: () => (/* binding */ main)\n/* harmony export */ });\n/* harmony import */ var _load_tab_one__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./load-tab-one */ \"./src/load-tab-one.js\");\n/* harmony import */ var _load_tab_two__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./load-tab-two */ \"./src/load-tab-two.js\");\n/* harmony import */ var _load_tab_three__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./load-tab-three */ \"./src/load-tab-three.js\");\n\n\n\n\nconst body = document.getElementsByTagName('body');\nconst header = document.getElementsByTagName('header');\nconst main = document.getElementsByTagName('main');\nconst footer = document.getElementsByTagName('footer');\n\nconst content = document.getElementById('content');\n\n_load_tab_one__WEBPACK_IMPORTED_MODULE_0__.pageLoad();\n\n\n\n//# sourceURL=webpack://generated-site-top/./src/index.js?");
 
 /***/ }),
 
@@ -26,7 +26,27 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _loa
   \*****************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   tabOne: () => (/* reexport module object */ _load_tab_one__WEBPACK_IMPORTED_MODULE_0__)\n/* harmony export */ });\n/* harmony import */ var _load_tab_one__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./load-tab-one */ \"./src/load-tab-one.js\");\nconst testAppend = document.createElement('div');\ntestAppend.innerHTML = `<p>Hello World</p>`;\ncontent.appendChild(testAppend);\n\nconsole.log('Hello World')\n\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvbG9hZC10YWItb25lLmpzLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vZ2VuZXJhdGVkLXNpdGUtdG9wLy4vc3JjL2xvYWQtdGFiLW9uZS5qcz9jNGE5Il0sInNvdXJjZXNDb250ZW50IjpbImNvbnN0IHRlc3RBcHBlbmQgPSBkb2N1bWVudC5jcmVhdGVFbGVtZW50KCdkaXYnKTtcbnRlc3RBcHBlbmQuaW5uZXJIVE1MID0gYDxwPkhlbGxvIFdvcmxkPC9wPmA7XG5jb250ZW50LmFwcGVuZENoaWxkKHRlc3RBcHBlbmQpO1xuXG5jb25zb2xlLmxvZygnSGVsbG8gV29ybGQnKVxuXG5leHBvcnQgKiBhcyB0YWJPbmUgZnJvbSAnLi9sb2FkLXRhYi1vbmUnOyJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./src/load-tab-one.js\n");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   pageLoad: () => (/* binding */ pageLoad)\n/* harmony export */ });\n/* harmony import */ var _index_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./index.js */ \"./src/index.js\");\n\n\nconst pageLoad = () => {\n\nconst testAppend = document.createElement('div');\ntestAppend.innerHTML = `<p>Hello World</p>`;\ncontent.appendChild(testAppend);\n\nconsole.log('Hello World');\n\n}\n\n\n\n//# sourceURL=webpack://generated-site-top/./src/load-tab-one.js?");
+
+/***/ }),
+
+/***/ "./src/load-tab-three.js":
+/*!*******************************!*\
+  !*** ./src/load-tab-three.js ***!
+  \*******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _index_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./index.js */ \"./src/index.js\");\n\n\n\n\n//# sourceURL=webpack://generated-site-top/./src/load-tab-three.js?");
+
+/***/ }),
+
+/***/ "./src/load-tab-two.js":
+/*!*****************************!*\
+  !*** ./src/load-tab-two.js ***!
+  \*****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   tabTwo: () => (/* reexport module object */ _load_tab_two__WEBPACK_IMPORTED_MODULE_1__)\n/* harmony export */ });\n/* harmony import */ var _index_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./index.js */ \"./src/index.js\");\n/* harmony import */ var _load_tab_two__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./load-tab-two */ \"./src/load-tab-two.js\");\n\n\n\n\n//# sourceURL=webpack://generated-site-top/./src/load-tab-two.js?");
 
 /***/ })
 
@@ -89,7 +109,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval-source-map devtool is used.
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
 /******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
 /******/ 	
 /******/ })()
